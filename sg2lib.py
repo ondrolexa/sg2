2d5c7e0e29f75eb85bf84fe112131f200a635fdb
Ondrej Lexa <lexa.ondrej@gmail.com>
2018-11-26 11:31:11 +0100

    Update

--- sg2lib.py
+++ sg2lib.py
@@ -12,17 +12,27 @@ def def_ellipse(F):
     x,y = dot(F, [xc,yc])
     plot(xc, yc, 'r', x, y, 'g')
     u, s, v = svd(F)
-    plot(x, y, 'k', lw=2)
     quiver(zeros(2), zeros(2),
                hstack((s*u[0],-s*u[0])), hstack((s*u[1],-s*u[1])),
                scale=1, units='xy')
-    axis('equal')
+
+def def_square(F):
+    # Draw square
+    xc, yc = [-1, -1, 1, 1, -1], [-1, 1, 1, -1, -1]
+    x,y = dot(F, [xc,yc])
+    plot(xc, yc, 'r', x, y, 'g')
 
 def dis_ellipse(J):
     # Draw strain ellipse from displacement gradient
     J = asarray(J)
     F = J + eye(2)
     def_ellipse(F)
+
+def dis_square(J):
+    # Draw square
+    J = asarray(J)
+    F = J + eye(2)
+    def_square(F)
 
 def dis_field(J):
     # Visualize displacement field from
@@ -31,7 +41,6 @@ def dis_field(J):
                     linspace(-2, 2, 17))
     u, v = tensordot(J, [X, Y], axes=1)
     quiver(X, Y, u, v, angles='xy')
-    axis('equal')
 
 def def_field(F):
     # Visualize displacement field from
@@ -44,14 +53,35 @@ def dis_show(J):
     # Draw displacement field and deformation ellipse
     # from displacement gradient
     dis_field(J)
+    dis_square(J)
     dis_ellipse(J)
+    title(f"J = [[{J[0,0]:g}, {J[0,1]:g}], [{J[1,0]:g}, {J[1,1]:g}]]")
+    axis('equal')
     show()
 
 def def_show(F):
     # Draw displacement field and deformation ellipse
     # from deformation gradient
     def_field(F)
+    def_square(F)
     def_ellipse(F)
+    axis('equal')
+    title(f"F = [[{F[0,0]:g}, {F[0,1]:g}], [{F[1,0]:g}, {F[1,1]:g}]]")
+    show()
+
+def vel_field(L, t=None):
+    # Visualize velocity field from
+    # spatial velocity gradient
+    X, Y = meshgrid(linspace(-2, 2, 17),
+                    linspace(-2, 2, 17))
+    u, v = tensordot(L, [X, Y], axes=1)
+    quiver(X, Y, u, v, angles='xy')
+    if t is not None:
+        F = la.expm(L*t)
+        def_square(F)
+        def_ellipse(F)
+    axis('equal', adjustable='box')
+    title('Spatial velocity gradient')
     show()
 
 def densify_list_xy(x, y, n=500, per=True):
